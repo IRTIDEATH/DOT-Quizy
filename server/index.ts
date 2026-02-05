91536a0f5330b6c4abb22cd7be9f04322097eda6
IRTIDEATH <enderjhass@gmail.com>
2026-02-05 17:46:17 +0700

    feat: add CORS and setting auth configuration

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,28 @@
-import { Hono } from 'hono'
-import { auth } from './lib/auth';
+import { Hono } from "hono";
+import { auth } from "./lib/auth";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono<{
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null;
+  };
+}>().basePath("/api");
 
-const router = app.on(["POST", "GET"], "/api/auth/*", (c) => {
-	return auth.handler(c.req.raw);
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL!,
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
+
+const router = app.on(["POST", "GET"], "/auth/*", (c) => {
+  return auth.handler(c.req.raw);
 });
 
-export type AppType = typeof router
-export default app
+export type AppType = typeof router;
+export default app;
--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -2,16 +2,40 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../database/db";
 import * as schema from "../database/schema";
-import { openAPI } from 'better-auth/plugins';
+import { admin, openAPI } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-    schema
+    schema,
   }),
   emailAndPassword: {
     enabled: true,
+    requireEmailVerification: false,
+    autoSignIn: false,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+        defaultValue: "user",
+        input: false,
+      },
+      premium: {
+        type: "boolean",
+        input: false,
+      },
+    },
   },
   trustedOrigins: [process.env.CLIENT_URL!],
-  plugins: [openAPI()],
+  plugins: [openAPI(), admin()],
+  advanced: {
+    database: {
+      generateId: "uuid",
+    },
+    crossSubDomainCookies: {
+      enabled: true,
+    },
+  },
 });
